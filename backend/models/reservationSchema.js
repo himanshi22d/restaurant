fccba6e858b63b090b768a6e5454b44a541347da
himanshi <himanshidabas22@gmail.com>
2024-04-19 14:25:43 +0530

    first commit

---
+++ backend/models/reservationSchema.js
@@ -0,0 +1,39 @@
+import mongoose  from "mongoose";
+import validator from "validator";
+
+const reservationSchema=new mongoose.Schema({  
+    firstName: {
+        type: String,
+        required: true,
+        minLength: [3," first name must contain atleast 3 characters!"],
+        maxLength: [30," first name cannot exceed 30 characters!"],
+     },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: [3," last name must contain atleast 3 characters!"],
+        maxLength: [30," last name cannot exceed 30 characters!"],
+    },
+    email:{
+        type: String,
+        required: true,
+        validate: [validator.isEmail, " provide a valid email!"],
+
+    },
+    phone: {
+        type: String,
+        required: true,
+        minLength: [10," phone number must contain only 10 digits "],
+        maxLength: [10," phone number must contain only 10 digits "],
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+});
+
+export const Reservation = mongoose.model("Reservation",reservationSchema);
